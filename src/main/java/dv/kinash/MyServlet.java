2eb1b7a5a0a06d114f6771f724b134b16a28a8ac
Dmitry Kinash <dv.kinash@gmail.com>
2023-04-03 20:31:33 +0300

    Initial commit

---
+++ src/main/java/dv/kinash/MyServlet.java
@@ -0,0 +1,26 @@
+package dv.kinash;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String mood = req.getParameter("mood");
+        String place = req.getHeader("place");
+
+        name = (name == null) ? "John Dou" : name;
+        mood = (mood == null) ? "happy" : mood;
+        place = (place == null) ? "London" : place;
+
+        resp.getWriter().println(
+                String.format("Hi, %1 from %2, you are felling %3", name, place, mood));
+    }
+}
